612b753bfe904c1f7dc639d852b3fc91464c3fa1
MohsenSami <mohsensami88@gmail.com>
2023-09-09 20:20:47 +0330

    basics

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,19 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        // options: {
+        //     storySort: (a, b) =>
+        //         a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+        // },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
     },
-  },
 };
 
 export default preview;
